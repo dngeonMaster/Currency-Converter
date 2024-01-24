1bb612bacdb046caaa31409f9829e7470afcf480
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-24 21:23:13 +0000

    //

---
+++ app.js
@@ -0,0 +1,10 @@
+const Base_url="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+
+
+const dropdowns=document.querySelectorAll(".dropdown select");
+
+for(let select of dropdowns){
+    for(code in countryList){
+        let newOption
+    }
+}
