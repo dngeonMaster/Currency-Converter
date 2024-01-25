569c9899a1bf2333aa77a37e2cc09dd11ea4a736
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-25 08:04:51 +0000

    ///

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ const Base_url="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/cu
 
 const dropdowns=document.querySelectorAll(".dropdown select");
 
+
 for(let select of dropdowns){
-    for(code in countryList){
-        let newOption
+    for(currCode in countryList){
+         let newOption=document.createElement("option");
+         newOption.innerText=currCode
+         newOption.value=currCode;
+         if(select.name==="from" &&currCode==="USD"){
+            newOption.selected="selected"
+         }
+         else if(select.name==="to" &&currCode==="INR"){
+            newOption.selected="selected"
+         }
+         select.append(newOption)
     }
+    
 }
+
+
+select.addEventListener("chnage",(evt)=>{
+    updateFlag(evt.target);
+});
+
+
+const updateFlag=(element)=>{
+console.log(element);
+} 
+
